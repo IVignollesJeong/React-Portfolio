c58c1a8f466704366f5c68d05da49d04e26e291c
Ian Vignolles-Jeong <ianvignolles@gmail.com>
2023-09-25 16:08:47 -0400

    add basic styling to app(), begin creating routes

--- IVPortfolio/src/App.jsx
+++ IVPortfolio/src/App.jsx
@@ -2,12 +2,33 @@ import { useState } from 'react'
 import './App.css'
 import About from './pages/About'
 import Navbar from './components/Navbar'
+import Portfolio from './pages/Portfolio'
+import Resume from './pages/Resume'
+import Contact from './pages/Contact'
 
 function App() {
+  let Component
+  switch (window.location.pathname) {
+    case "/":
+      Component = About
+      break
+    case "/portfolio":
+      Component = Portfolio
+      break
+    case "/resume":
+      Component = Resume
+      break
+    case "/contact":
+      Component = Contact
+      break
+  }
 
   return (
     <>
       <Navbar />
+      <div className="container">
+      <Component />
+      </div>
     </>
   )
 }
